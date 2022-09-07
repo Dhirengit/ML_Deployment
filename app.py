d0a14e4accd4d83db09e370cf28250f7eb04cf36
Dhiren Patel <dhirenpateldb@gmail.com>
2022-09-07 22:58:40 +0530

    First Commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-# importing the necessary dependencies 
+# importing the necessary dependencies
 from flask import Flask, render_template, request,jsonify
 from flask_cors import CORS,cross_origin
 import pickle
